0a6ae48fa44f8990db3f85576af37bad279f8407
Andy Chapman <achapm87@gmail.com>
2025-02-19 23:40:03 -0500

    Define tailwind breakpoints & add indicator component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { BreakpointIndicator } from "@/components/BreakpointIndicator";
 import "@/styles/app.css";
 
 const geistSans = Geist({
@@ -27,6 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <BreakpointIndicator />
         {children}
       </body>
     </html>
---
+++ src/components/BreakpointIndicator.tsx
@@ -0,0 +1,14 @@
+export function BreakpointIndicator() {
+  return (
+    process.env.NODE_ENV === 'development' && (
+      <div className="fixed left-0 top-0 z-[100000] flex w-[30px] items-center justify-center bg-gray-200 py-[2.5px] text-[12px] uppercase text-black sm:bg-red-200 md:bg-yellow-200 lg:bg-green-200 xl:bg-blue-200 2xl:bg-pink-200">
+        <span className="block xs:hidden">all</span>
+        <span className="hidden xs:block sm:hidden">xs</span>
+        <span className="hidden sm:block md:hidden">sm</span>
+        <span className="hidden md:block lg:hidden">md</span>
+        <span className="hidden lg:block xl:hidden">lg</span>
+        <span className="hidden xl:block 2xl:hidden">xl</span>
+        <span className="hidden 2xl:block">2xl</span>
+      </div>
+  ))
+}
