6c39c2fe91d3366bf53b2e33f4e48163b89bd668
Andy Chapman <achapm87@gmail.com>
2025-02-21 00:00:01 -0500

    Switch to Slate theme

--- src/components/BreakpointIndicator.tsx
+++ src/components/BreakpointIndicator.tsx
@@ -1,7 +1,7 @@
-export function BreakpointIndicator() {
+export default function BreakpointIndicator() {
   return (
     process.env.NODE_ENV === 'development' && (
-      <div className="fixed left-0 top-0 z-[100000] flex w-[30px] items-center justify-center bg-gray-200 py-[2.5px] text-[12px] uppercase text-black sm:bg-red-200 md:bg-yellow-200 lg:bg-green-200 xl:bg-blue-200 2xl:bg-pink-200">
+      <div className="fixed left-0 top-0 z-[100000] flex w-[30px] items-center justify-center bg-slate-200 py-[2.5px] text-[12px] uppercase text-black sm:bg-red-200 md:bg-yellow-200 lg:bg-green-200 xl:bg-blue-200 2xl:bg-pink-200">
         <span className="block xs:hidden">all</span>
         <span className="hidden xs:block sm:hidden">xs</span>
         <span className="hidden sm:block md:hidden">sm</span>
--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -24,13 +24,13 @@ export default function Header() {
 
   return (
     <>
-      <nav className="header bg-gray-800 relative">
-        <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+      <nav className="header bg-slate-900 relative">
+        <div className="mx-auto container px-3 sm:px-4">
           <div className="relative flex h-16 items-center justify-between">
             <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
               {/* Mobile menu button */}
               <button type="button"
-                className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-white focus:outline-hidden focus:ring-inset cursor-pointer"
+                className="relative inline-flex items-center justify-center rounded-md p-2 text-slate-400 hover:bg-slate-700 hover:text-white focus:ring-2 focus:ring-white focus:outline-hidden focus:ring-inset cursor-pointer"
                 aria-controls="mobile-menu"
                 aria-expanded="false"
                 onClick={() => setMobileMenuActive(!mobileMenuActive)}
@@ -55,7 +55,7 @@ export default function Header() {
               </div>
             </div>
             <div className="header-right absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-              <button type="button" className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden">
+              <button type="button" className="relative rounded-full bg-slate-900 p-1 text-slate-400 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-slate-900 focus:outline-hidden">
                 <span className="absolute -inset-1.5"></span>
                 <span className="sr-only">View notifications</span>
                 <Bell />
@@ -65,7 +65,7 @@ export default function Header() {
               <div className="relative ml-3">
                 <div>
                   <button type="button"
-                    className="relative flex rounded-full bg-gray-800 text-sm focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden cursor-pointer"
+                    className="relative flex rounded-full bg-slate-900 text-sm focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-slate-900 focus:outline-hidden cursor-pointer"
                     id="user-menu-button"
                     aria-expanded="false"
                     aria-haspopup="true"
@@ -73,14 +73,14 @@ export default function Header() {
                   >
                     <span className="absolute -inset-1.5"></span>
                     <span className="sr-only">Open user menu</span>
-                    {images.length > 0 && <Image className="rounded-full" src={images[0].url} alt={display_name} width={32}  height={32} />}
+                    {(images && images.length > 0) && <Image className="rounded-full" src={images[0].url} alt={display_name} width={32}  height={32} />}
                   </button>
                 </div>
                 <div className={`user-menu absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 ring-1 shadow-lg ring-black/5 focus:outline-hidden${userMenuActive ? ' block' : ' hidden'}`} role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabIndex={-1}>
                   <div className='flex flex-col'>
-                    <span className="block px-4 py-2 text-sm text-gray-700" role="menuitem">Logged in as: {display_name}</span>
-                    <span className="block px-4 py-2 text-sm text-gray-700" role="menuitem">Email: {email}</span>
-                    <a href={href} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem" target='_blank'>Your Profile</a>
+                    <span className="block px-4 py-2 text-sm text-slate-700" role="menuitem">Logged in as: {display_name}</span>
+                    <span className="block px-4 py-2 text-sm text-slate-700" role="menuitem">Email: {email}</span>
+                    <a href={href} className="block px-4 py-2 text-sm text-slate-700 hover:bg-slate-100" role="menuitem" target='_blank'>Your Profile</a>
                   </div>
                 </div>
               </div>
@@ -89,9 +89,9 @@ export default function Header() {
         </div>
 
         {/* Mobile menu, show/hide based on menu state. */}
-        <div className={`header-nav bg-gray-800 absolute top-full w-full height-full ${mobileMenuActive ? 'block' : 'hidden'} sm:hidden`} id="mobile-menu">
+        <div className={`header-nav bg-slate-900 absolute top-full w-full height-full ${mobileMenuActive ? 'block' : 'hidden'} sm:hidden`} id="mobile-menu">
           <div className="space-y-1 px-2 pt-2 pb-3">
-            {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
+            {/* Current: "bg-slate-900 text-white", Default: "text-slate-300 hover:bg-slate-700 hover:text-white" */}
             <a href="#" className="nav-link current block rounded-md px-3 py-2 text-base font-medium" aria-current="page">Dashboard</a>
           </div>
         </div>
